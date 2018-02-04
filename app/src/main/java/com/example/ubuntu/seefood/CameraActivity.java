65b22e26bde8c556198bb4549efbfd8f9d35d7b9
Himanshu Rawlani <himanshurawlani@hotmail.com>
2018-02-04 20:00:54 +0530

    Added camera preview

--- app/src/main/java/com/example/ubuntu/seefood/CameraActivity.java
+++ app/src/main/java/com/example/ubuntu/seefood/CameraActivity.java
@@ -3,25 +3,51 @@ package com.example.ubuntu.seefood;
 import android.Manifest;
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Trace;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.util.Size;
 import android.view.View;
 import android.widget.Toast;
+
+import com.example.ubuntu.seefood.env.Logger;
+import com.example.ubuntu.seefood.env.ImageUtils;
+
+import java.nio.ByteBuffer;
+
 
 /**
  * Created by ubuntu on 3/2/18.
  */
 
 /** Main {@code Activity} class for the Camera app. */
-public class CameraActivity extends Activity {
+public class CameraActivity extends Activity implements ImageReader.OnImageAvailableListener {
 
+    private static final Logger LOGGER = new Logger();
     private static final int PERMISSIONS_REQUEST = 1;
     private static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
     private View mLayout;
+    private boolean useCamera2API;
+    private static final Size DESIRED_PREVIEW_SIZE = new Size(640, 480);
+    protected int previewWidth = 0;
+    protected int previewHeight = 0;
+    private boolean isProcessingFrame = false;
+    private byte[][] yuvBytes = new byte[3][];
+    private int[] rgbBytes = null;
+    private int yRowStride;
+    private Runnable postInferenceCallback;
+    private Runnable imageConverter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +56,7 @@ public class CameraActivity extends Activity {
         mLayout = findViewById(R.id.main_layout);
 
         if (hasPermission()) {
-            // setFragment();
-            Snackbar.make(mLayout,
-                    "Camera permission is available.",
-                    Snackbar.LENGTH_SHORT).show();
+            setFragment();
         } else {
             requestPermission();
         }
@@ -63,6 +86,7 @@ public class CameraActivity extends Activity {
                 Snackbar.make(mLayout, "Camera permission was granted.",
                         Snackbar.LENGTH_SHORT)
                         .show();
+                setFragment();
             } else {
                 // Permission request was denied.
                 Snackbar.make(mLayout, "Camera permission request was denied.",
@@ -96,5 +120,172 @@ public class CameraActivity extends Activity {
         }
     }
 
+    /****************** Code to display camera preview in FrameLayout **********************/
+    protected void setFragment() {
+        String cameraId = chooseCamera();
+
+        Fragment fragment;
+        if (useCamera2API) {
+            CameraConnectionFragment camera2Fragment =
+                    CameraConnectionFragment.newInstance(
+                            new CameraConnectionFragment.ConnectionCallback() {
+                                @Override
+                                public void onPreviewSizeChosen(final Size size, final int rotation) {
+                                    previewHeight = size.getHeight();
+                                    previewWidth = size.getWidth();
+//                                    CameraActivity.this.onPreviewSizeChosen(size, rotation);
+                                }
+                            },
+                            this,
+                            getLayoutId(),
+                            getDesiredPreviewFrameSize());
+
+            camera2Fragment.setCamera(cameraId);
+            fragment = camera2Fragment;
+        }
+        else {
+//            fragment =
+//                    new LegacyCameraConnectionFragment(this, getLayoutId(), getDesiredPreviewFrameSize());
+            fragment=null;
+            Log.d("CameraActivity.java","setFragment(): fragment not initialised!");
+        }
+
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+    }
+
+    private String chooseCamera() {
+        final CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            for (final String cameraId : manager.getCameraIdList()) {
+                final CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+
+                // We don't use a front facing camera in this sample.
+                final Integer facing = characteristics.get(CameraCharacteristics.LENS_FACING);
+                if (facing != null && facing == CameraCharacteristics.LENS_FACING_FRONT) {
+                    continue;
+                }
+
+                final StreamConfigurationMap map =
+                        characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+
+                if (map == null) {
+                    continue;
+                }
+
+                // Fallback to camera1 API for internal cameras that don't have full support.
+                // This should help with legacy situations where using the camera2 API causes
+                // distorted or otherwise broken previews.
+                useCamera2API = (facing == CameraCharacteristics.LENS_FACING_EXTERNAL)
+                        || isHardwareLevelSupported(characteristics,
+                        CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_FULL);
+                LOGGER.i("Camera API lv2?: %s", useCamera2API);
+                return cameraId;
+            }
+        } catch (CameraAccessException e) {
+            LOGGER.e(e, "Not allowed to access camera");
+        }
+
+        return null;
+    }
+
+    // Returns true if the device supports the required hardware level, or better.
+    private boolean isHardwareLevelSupported(
+            CameraCharacteristics characteristics, int requiredLevel) {
+        int deviceLevel = characteristics.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL);
+        if (deviceLevel == CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_LEGACY) {
+            return requiredLevel == deviceLevel;
+        }
+        // deviceLevel is not LEGACY, can use numerical sort
+        return requiredLevel <= deviceLevel;
+    }
+
+    @Override
+    public void onImageAvailable(ImageReader imageReader) {
+        //We need wait until we have some size from onPreviewSizeChosen
+        if (previewWidth == 0 || previewHeight == 0) {
+            return;
+        }
+        if (rgbBytes == null) {
+            rgbBytes = new int[previewWidth * previewHeight];
+        }
+        try {
+            final Image image = imageReader.acquireNextImage();
+
+            if (image == null) {
+                return;
+            }
+
+            if (isProcessingFrame) {
+                image.close();
+                return;
+            }
+            isProcessingFrame = true;
+            Trace.beginSection("imageAvailable");
+            final Image.Plane[] planes = image.getPlanes();
+            fillBytes(planes, yuvBytes);
+            yRowStride = planes[0].getRowStride();
+            final int uvRowStride = planes[1].getRowStride();
+            final int uvPixelStride = planes[1].getPixelStride();
+
+            imageConverter =
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            ImageUtils.convertYUV420ToARGB8888(
+                                    yuvBytes[0],
+                                    yuvBytes[1],
+                                    yuvBytes[2],
+                                    previewWidth,
+                                    previewHeight,
+                                    yRowStride,
+                                    uvRowStride,
+                                    uvPixelStride,
+                                    rgbBytes);
+                        }
+                    };
+
+            postInferenceCallback =
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            image.close();
+                            isProcessingFrame = false;
+                        }
+                    };
+
+            //processImage();
+        } catch (final Exception e) {
+            LOGGER.e(e, "Exception!");
+            Trace.endSection();
+            return;
+        }
+        Trace.endSection();
+    }
+
+    protected void fillBytes(final Image.Plane[] planes, final byte[][] yuvBytes) {
+        // Because of the variable row stride it's not possible to know in
+        // advance the actual necessary dimensions of the yuv planes.
+        for (int i = 0; i < planes.length; ++i) {
+            final ByteBuffer buffer = planes[i].getBuffer();
+            if (yuvBytes[i] == null) {
+                LOGGER.d("Initializing buffer %d at size %d", i, buffer.capacity());
+                yuvBytes[i] = new byte[buffer.capacity()];
+            }
+            buffer.get(yuvBytes[i]);
+        }
+    }
+
+    /****************** Methods to be implemented by DetectorActivity.java ********************/
+    //protected abstract void processImage();
+    //protected abstract void onPreviewSizeChosen(final Size size, final int rotation);
+    protected int getLayoutId(){
+        return R.layout.camera_connection_fragment_tracking;
+    }
+    protected Size getDesiredPreviewFrameSize(){
+        return DESIRED_PREVIEW_SIZE;
+    }
 
 }
---
+++ app/src/main/java/com/example/ubuntu/seefood/OverlayView.java
@@ -0,0 +1,44 @@
+package com.example.ubuntu.seefood;
+
+import android.view.View;
+
+/**
+ * Created by ubuntu on 3/2/18.
+ */
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * A simple View providing a render callback to other classes.
+ */
+public class OverlayView extends View {
+    private final List<DrawCallback> callbacks = new LinkedList<DrawCallback>();
+
+    public OverlayView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /**
+     * Interface defining the callback for client classes.
+     */
+    public interface DrawCallback {
+        public void drawCallback(final Canvas canvas);
+    }
+
+    public void addCallback(final DrawCallback callback) {
+        callbacks.add(callback);
+    }
+
+    @Override
+    public synchronized void draw(final Canvas canvas) {
+        super.draw(canvas);
+        for (final DrawCallback callback : callbacks) {
+            callback.drawCallback(canvas);
+        }
+    }
+}
